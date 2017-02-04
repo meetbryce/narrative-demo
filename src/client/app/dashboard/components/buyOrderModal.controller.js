72fae791845057f99fac706c30b6c25167466c3e
Bryce York <bryce@meetbryce.com>
2017-02-03 16:05:55 -0800

    make buy order modal multi-purpose - Create & Edit

--- src/client/app/dashboard/components/buyOrderModal.controller.js
+++ src/client/app/dashboard/components/buyOrderModal.controller.js
@@ -9,15 +9,14 @@
         '$log',
         '$mdDialog',
         '$timeout',
+        'data',
     ];
 
     /* @ngInject */
-    function BuyOrderModalController ($log, $mdDialog, $timeout) {
+    function BuyOrderModalController ($log, $mdDialog, $timeout, data) {
         const $ctrl = this;
         $ctrl.cancel = () => $mdDialog.cancel();
         $ctrl.state = {};
-        $ctrl.state.title = 'Place A New Buy Order';
-        $ctrl.buyOrder = {};
 
         activate();
 
@@ -26,6 +25,15 @@
         function activate () {
             $log.info('Running', $ctrl.state.title);
             $ctrl.state.loading = true;
+            if (data) {
+                $ctrl.buyOrder = data;
+                $ctrl.state.title = 'Editing Buy Order';
+                $ctrl.state.buttonLabel = 'Save Changes';
+            } else {
+                $ctrl.buyOrder = {};
+                $ctrl.state.title = 'Place A New Buy Order';
+                $ctrl.state.buttonLabel = 'Place The Buy Order';
+            }
             $timeout(() => { $ctrl.state.loading = false; }, 450);
         }
     }
